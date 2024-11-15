1d2ad321615181f2050b4546c804f0936679ea6e
NikShash <myksha-ipt23@lll.kpi.ua>
2024-11-15 15:14:11 +0200

    Horner scheme added

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 class GF2Polynomial:
     def __init__(self, coeffs):
-       # self.coeffs = np.array(coeffs, dtype=np.int8) % 2
         self.coeffs = np.array(coeffs, dtype=np.int8) & 1
         self.trim()
 
@@ -19,7 +18,7 @@ class GF2Polynomial:
         padded_other = np.pad(other.coeffs, (max_len - len(other.coeffs), 0))
         result_coeffs = np.bitwise_xor(padded_self, padded_other)
         return GF2Polynomial(result_coeffs)
-    
+
     def __mul__(self, other):
         result_coeffs = np.zeros(len(self.coeffs) + len(other.coeffs) - 1, dtype=np.int8)
         for i in range(len(self.coeffs)):
@@ -35,18 +34,35 @@ class GF2Polynomial:
         while len(coeffs) >= len(generator_polynomial.coeffs):
             if coeffs[0] == 1:
                 coeffs[:len(generator_polynomial.coeffs)] ^= generator_polynomial.coeffs
-            coeffs = np.trim_zeros(coeffs, 'f') or np.array([0], dtype=np.int8)
+            coeffs = np.trim_zeros(coeffs, 'f')
+            if coeffs.size == 0:
+                coeffs = np.array([0], dtype=np.int8)
         return GF2Polynomial(coeffs)
 
     def __repr__(self):
         return f"GF2Polynomial({self.coeffs.tolist()})"
+    
+    def power(self, exp):
+        result = GF2Polynomial([1])
+        base = self
+        
+        while exp > 0:
+            if exp % 2 == 1:
+                result = result * base
+            base = base.square()
+            exp //= 2
+        
+        return result
 
 generator_coeffs_str = os.getenv('GENERATOR_POLYNOMIAL')
-generator_coeffs     = np.array(eval(os.getenv("GENERATOR_POLYNOMIAL")), dtype=np.int8)
+generator_coeffs = np.array(eval(os.getenv("GENERATOR_POLYNOMIAL")), dtype=np.int8)
 generator_polynomial = GF2Polynomial(generator_coeffs)
 
 poly = GF2Polynomial([1, 1, 1])
 squared_result = poly.square()
-squared_resul_multiply = poly * poly
+squared_result_multiply = poly * poly
+power_result = poly.power(2**491 - 2)
+
 print("Result of squaring:", squared_result)
-print("Result of squaring:", squared_resul_multiply)
+print("Result of squaring (multiplication):", squared_result_multiply)
+print("Result of raising to the power of 2^251:", power_result)
